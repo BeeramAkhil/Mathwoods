6ff636169b9262a4fbfb5f7b03d9b45cba5c1a67
user <user@DESKTOP-NBMPK5C>
2022-05-04 23:28:57 -0700

    04052022

---
+++ src/test/java/BHigh/Aboutto.java
@@ -0,0 +1,19 @@
+package BHigh;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class Aboutto {
+  @Test
+  public void f() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
